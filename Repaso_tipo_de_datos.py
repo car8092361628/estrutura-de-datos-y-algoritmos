b8e147fda90f87c1a95a2bc4ab4b980a6e557952
car8092361628 <carlosmiguelgaarcia1991>
2025-03-20 19:42:18 -0400

    Repaso_TD

---
+++ Repaso_tipo_de_datos.py
@@ -0,0 +1,26 @@
+#Trabajando con tipos de datos entero
+
+edad=25
+print("mi edad es:", edad)
+
+#Trabajando con float
+pi=3.14
+print("Imprimir valor: ",pi)
+
+#Trabajando con complex
+
+Ecuacion=1+2j
+print("Mi ecuacion es: ",Ecuacion)
+
+#Trabajando con string
+
+saludo="Hola Mundo"
+
+print("saludos:",saludo)
+
+#Trabajando con booleanos
+
+es_verdadero=True
+es_falso=False
+print("tipo de valor",es_verdadero)
+print("tipo de valor",es_falso)

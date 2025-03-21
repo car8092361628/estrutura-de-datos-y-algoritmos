d8b9743148867293715812ef7b2b7069fb6fb4df
car8092361628 <carlosmiguelgaarcia1991>
2025-03-20 20:13:20 -0400

    repaso estrutura condicionales y repetitivas.

---
+++ Est.Rep_COnd.py
@@ -0,0 +1,33 @@
+##Estructura condicionales y repetitiva
+# if else, if-else
+#for, while ,swich
+
+edad=18
+
+if edad <18:
+    print(f"soy menor de edad：{edad}")
+elif edad==18:
+    print("Acabas de cumplir 18 años.") 
+else:
+    print("Eres mayor de edad")
+    
+    
+    #for
+    
+fruta=["manzana","guineo","uva","pera","pina"]
+
+for fruta in fruta:
+    print("Imprime todos los elemento de la lista: ",fruta)
+    
+#Bucle while   
+ 
+contando=0
+
+while contando <6:
+    print("Contador",contando)
+    contando+=1
+
+
+
+    
+        

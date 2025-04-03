cdc5f669188c9120288246393f2ff160770318ef
car8092361628 <carlosmiguelgaarcia1991>
2025-04-02 20:01:09 -0400

    update

---
+++ Ejercicios Grupo 1/FuncParam.py
@@ -0,0 +1,6 @@
+def agregar(a,b):
+    return a + b
+
+#agregar(5,6)
+print(agregar(5,6))
+    
---
+++ Ejercicios Grupo 1/defPara2.py
@@ -0,0 +1,5 @@
+def multiplicar(x, y):
+    return x * y
+
+resultado=multiplicar(5, 10)
+print(resultado)

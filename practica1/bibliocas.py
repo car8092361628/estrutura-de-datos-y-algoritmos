b7e49d7ba9f172c2bc4c63736ea530ea55c1d6bc
car8092361628 <carlosmiguelgaarcia1991>
2025-04-15 18:15:17 -0400

    update

--- practica1/bibliocas.py
+++
@@ -1,5 +0,0 @@
-import math
-
-numero=16
-raiz=math.sqrt(numero)
-print("La raiz cuadrada de 16 es: ",raiz)
--- practica2/FunPara.py
+++
@@ -1,5 +0,0 @@
-def agregar(a,b):#parametros
-    return a+b
-
-
-print (agregar(2,3))#argumento
